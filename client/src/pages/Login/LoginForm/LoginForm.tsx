66d6ebc3d756f76925038b23f5c386c33830a82d
Victor Scherman <vicscherman@gmail.com>
2021-06-01 00:21:54 -0400

    Update LoginForm.tsx

--- client/src/pages/Login/LoginForm/LoginForm.tsx
+++ client/src/pages/Login/LoginForm/LoginForm.tsx
@@ -90,6 +90,20 @@ export default function Login({ handleSubmit }: Props): JSX.Element {
               {isSubmitting ? <CircularProgress style={{ color: 'white' }} /> : 'SIGN IN'}
             </Button>
           </Box>
+          <Box textAlign="center">
+            <Button
+              type="submit"
+              onClick={() => {
+                (values.email = 'testUser@gmail.com'), (values.password = 'test123');
+              }}
+              size="large"
+              variant="contained"
+              color="primary"
+              className={classes.submit}
+            >
+              {isSubmitting ? <CircularProgress style={{ color: 'white' }} /> : 'USE DEMO ACCOUNT'}
+            </Button>
+          </Box>
           <div style={{ height: 95 }} />
         </form>
       )}
