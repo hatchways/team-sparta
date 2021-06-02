69db0ff828e06cf7d8b3d6b9c27f31fad58dd8d9
Milad Sakhizadah <milad9890@gmail.com>
2021-06-02 16:11:05 -0400

    fixed typos in submission model

--- server/models/Submission.js
+++ server/models/Submission.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const submissionSchema = new mongoose.Schema({
   creator: { type: mongoose.Types.ObjectId, required: true, ref: "User" },
-  contest_id: { type: mongoose.Types.ObjectId, required: true, ref: "User" },
+  contest_id: { type: mongoose.Types.ObjectId, required: true, ref: "Contest" },
   files: [{ type: String, required: true }],
   is_active: { type: Boolean, default: true },
   date_created: { type: Date, default: Date.now },
