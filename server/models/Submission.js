99545208b2c1c60c9d4aebaf289dba2dc53fbb2b
Milad Sakhizadah <milad9890@gmail.com>
2021-06-02 16:08:02 -0400

    created submission model

---
+++ server/models/Submission.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const submissionSchema = new mongoose.Schema({
+  creator: { type: mongoose.Types.ObjectId, required: true, ref: "User" },
+  contest_id: { type: mongoose.Types.ObjectId, required: true, ref: "User" },
+  files: [{ type: String, required: true }],
+  is_active: { type: Boolean, default: true },
+  date_created: { type: Date, default: Date.now },
+});
+
+module.exports = Contest = mongoose.model("Submission", submissionSchema);
