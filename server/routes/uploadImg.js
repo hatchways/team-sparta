1d6f286da8ba7a7ee028b680b3e9cde3e26ae34d
Victor Scherman <vicscherman@gmail.com>
2021-06-02 21:25:59 -0400

    Set up AWS S3
    /utils/uploadFiles.js
    -validates file type, pushes to aws, returns key value pair
    
    routes/uploadImg.js creates route for images upload

---
+++ server/routes/uploadImg.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const router = express.Router();
+const uploadMultiImage = require("../utils/uploadFiles");
+
+// image upload route
+
+router.post("/images-upload", (req, res) => {
+  uploadMultiImage(req, res, (error) => {
+    console.log("uploaded files", req.files);
+    if (error) {
+      console.log("errors", error);
+      // we can use this to display error notifications
+      res.json({ error: error });
+    } else {
+      //if files not found
+      if (req.files === undefined) {
+        console.log("ERROR: No file selected");
+        res.json("Error: No file selected");
+      } else {
+        // if successful
+        let fileArray = req.files,
+          fileLocation;
+        const multiImageLocationArray = [];
+        for (let i = 0; i < fileArray.length; i++) {
+          fileLocation = fileArray[i].fileLocation;
+          console.log("FILE URL", fileLocation);
+          multiImageLocationArray.push(fileLocation);
+        }
+        // Here we could save the file name to the database
+        res.json({
+          filesArray: filesArray,
+          locationArray: multiImageLocationArray,
+        });
+      }
+    }
+  });
+});
+
+module.exports = router;
