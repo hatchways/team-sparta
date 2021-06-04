0c6ed81d756dedba2ce8b00a153198705780e8bd
Kevin Pham <kqpham22@gmail.com>
2021-06-04 14:21:46 -0400

    Fix caused additional bugs reapplied previous commit

--- client/src/context/useAuthContext.tsx
+++ client/src/context/useAuthContext.tsx
@@ -46,7 +46,7 @@ export const AuthProvider: FunctionComponent = ({ children }): JSX.Element => {
       await loginWithCookies().then((data: AuthApiData) => {
         if (data.success) {
           updateLoginContext(data.success);
-          history.goBack();
+          history.push('/');
         } else {
           // don't need to provide error feedback as this just means user doesn't have saved cookies or the cookies have not been authenticated on the backend
           setLoggedInUser(null);
