9b79f829f0abbf2d71ecdf041f2f9014f296a5d9
MiladS95 <35964452+MiladS95@users.noreply.github.com>
2021-06-11 17:10:23 -0400

    Update Contest.ts

--- client/src/interface/Contest.ts
+++ client/src/interface/Contest.ts
@@ -27,14 +27,3 @@ export interface Contest {
   submissions: string[];
 
 }
-export interface Contest {
-  _id: string;
-  title: string;
-  description: string;
-  price: number;
-  start_date: Date | string;
-  end_date: Date | string;
-  creator: string;
-  images: string[];
-  submissions: string[];
-}
